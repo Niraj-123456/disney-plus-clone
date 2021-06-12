81d0edf43ba9c3ae25f1a6c558c4e322271c17a8
Niraj-123456 <lamaniraj321@gmail.com>
2021-06-12 15:36:21 +0545

    changes to package.json file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzGBDGERJkeY",                             // Auth / General Use
-    appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",              // General Use
+    apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY", // Auth / General Use
+    appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",  // General Use
     projectId: "disneyplus-clone-a33d5",               // General Use
-    authDomain: "disneyplus-clone-a33d5.firebaseapp.com",         // Auth with popup/redirect
-    storageBucket: "disneyplus-clone-a33d5.appspot.com",          // Storage
-    messagingSenderId: "37918794208",                 // Cloud Messaging
-    measurementId: "G-DRVLJKWRWG"                        // Analytics
+    authDomain: "disneyplus-clone-a33d5.firebaseapp.com", // Auth with popup/redirect
+    storageBucket: "disneyplus-clone-a33d5.appspot.com",  // Storage
+    messagingSenderId: "37918794208", // Cloud Messaging
+    measurementId: "G-DRVLJKWRWG" // Analytics
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
