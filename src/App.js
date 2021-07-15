e8616986cba19437c8888aa369362dd6df487b99
Niraj-123456 <lamaniraj321@gmail.com>
2021-07-15 19:33:16 +0545

    added login and logout method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header.js'
-import Home from './components/Home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Detail from './components/Detail'
-import Login from './components/Login'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -17,13 +12,13 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/detail/:id'>
+          <Route path="/detail/:id">
             <Detail />
           </Route>
-          <Route path='/'>
+          <Route path="/">
             <Home />
           </Route>
         </Switch>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,105 +1,184 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { auth, provider } from "../firebase";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLogin,
+  setSignout,
+} from "../features/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function Header() {
-    return (
-        <Nav>
-            <Logo src="/images/logo.svg" />
-            <NavMenu>
-                <a>
-                    <img src="/images/home-icon.svg" />
-                    <span>Home</span>
-                </a>
-                <a>
-                    <img src="/images/search-icon.svg" />
-                    <span>SEARCH</span>
-                </a>
-                <a>
-                    <img src="/images/watchlist-icon.svg" />
-                    <span>WATCHLIST</span>
-                </a>
-                <a>
-                    <img src="/images/original-icon.svg" />
-                    <span>ORIGINALS</span>
-                </a>
-                <a>
-                    <img src="/images/movie-icon.svg" />
-                    <span>MOVIES</span>
-                </a>
-                <a>
-                    <img src="/images/series-icon.svg" />
-                    <span>SERIES</span>
-                </a>
-            </NavMenu>
-            <UserImg src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png" />
-        </Nav>
-    )
+  const [user, setUser] = useState("");
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+  const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  }, []);
+  const signIn = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      let user = result.user;
+      dispatch(
+        setUserLogin({
+          name: user.displayName,
+          email: user.email,
+          photo: user.photoURL,
+        })
+      );
+      history.push("/");
+    });
+  };
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setSignout);
+      history.push("/login");
+    });
+  };
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      {!userName ? (
+        <LoginContainer>
+          <Login onClick={signIn}>Login</Login>
+        </LoginContainer>
+      ) : (
+        <>
+          <NavMenu>
+            <a>
+              <img src="/images/home-icon.svg" />
+              <span>Home</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+
+          <UserImg
+            src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png"
+            onClick={signOut}
+          />
+        </>
+      )}
+    </Nav>
+  );
 }
 
-export default Header
+export default Header;
 
 const Nav = styled.nav`
-    height: 70px;
-    background: #090b13;
-    display: flex;
-    align-items: center;
-    padding: 0 36px;
-    overflow-x: hidden;
-`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  overflow-x: hidden;
+`;
 
 const Logo = styled.img`
-    width: 80px;
-    margin-right: 25px;
-`
+  width: 80px;
+  margin-right: 25px;
+`;
 
 const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-right: 25px;
+  align-items: center;
+  a {
     display: flex;
-    flex: 1;
-    margin-right: 25px;
     align-items: center;
-    a {
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
-        cursor: pointer;
+    padding: 0 12px;
+    cursor: pointer;
 
-        img {
-            height: 20px;
-            padding-right: 2px;
-        }
-
-        span{
-            font-size: 13px;
-            letter-spacing: 1.42px;
-            position: relative;
-
-            &:after {
-                content: "";
-                height: 2px;
-                background: white;
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: -6px;
-                opacity: 0;
-                transform-origin: left center;
-                transform: scaleX(0);
-                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-            }
-        }
-
-        &:hover {
-            span:after {
-                transform: scaleX(1);
-                opacity: 1;
-            }
-        }
+    img {
+      height: 20px;
+      padding-right: 2px;
     }
 
-`
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
 
 const UserImg = styled.img`
-    height: 40px;
-    width: 40px;
-    border-radius: 50%;
-`
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const Login = styled.div`
+  border: 1px solid #f9f9f9;
+  padding: 8px 16px;
+  border-radius: 4px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`;
